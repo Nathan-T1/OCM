4c375d38aa3dbf5cbfdff3507101a2449092393b
Naathan Tormaschy <ntormaschy@wisc.edu>
2021-02-15 16:51:12 -0600

    Added Risk Class

--- ALGO/ALGO.cpp
+++ ALGO/ALGO.cpp
@@ -23,9 +23,36 @@ struct PATH_STRUCT
 
 };
 
-class CERBERUS
+class CERBERUS_RISK
 {
 	public:
+		static double marginRate;
+		int openTradeCount, pendingOrderCount;
+		int BROKERAGE;
+		double balance, pl;
+
+		CERBERUS_RISK(int BROKERAGE);
+		std::string init_risk(std::string ACCOUNT_PATH, std::string API_TOKEN);
+
+};
+
+CERBERUS_RISK::CERBERUS_RISK(int BROKERAGE_INPUT)
+{
+	
+	BROKERAGE = BROKERAGE_INPUT;
+	
+}
+
+std::string CERBERUS_RISK::init_risk(std::string ACCOUNT_PATH, std::string API_TOKEN)
+{
+	std::string response = DownloadJSON(ACCOUNT_PATH, API_TOKEN);
+	return response;
+};
+
+class CERBERUS: public CERBERUS_RISK
+{
+	public:
+		
 		int BROKERAGE, LIVE_DEMO;
 		std::string BROKERAGE_STR, URL, ACCOUNT_ID, API_TOKEN;
 		PATH_STRUCT PATH_STRUCT;
@@ -35,31 +62,7 @@ class CERBERUS
 
 };
 
-void CERBERUS::set_info() 
-{
-	std::ifstream input(info_path);
-	std::string file;
-	int i = 0;
-
-	switch (BROKERAGE)
-	{
-	case 0:
-		for (std::string line; std::getline(input, line);)
-		{
-			if (i == 0)
-			{
-				ACCOUNT_ID = line;
-			}
-			else if (i == 1)
-			{
-				API_TOKEN = line;
-			}
-			i++;
-		}
-	}
-}
-
-CERBERUS::CERBERUS(int BROKERAGE_INPUT, int LIVE_DEMO_INPUT) 
+CERBERUS::CERBERUS(int BROKERAGE_INPUT, int LIVE_DEMO_INPUT) : CERBERUS_RISK(BROKERAGE_INPUT)
 {
 	BROKERAGE = BROKERAGE_INPUT;
 	LIVE_DEMO = LIVE_DEMO_INPUT;
@@ -87,17 +90,42 @@ CERBERUS::CERBERUS(int BROKERAGE_INPUT, int LIVE_DEMO_INPUT)
 	}
 }
 
+void CERBERUS::set_info() 
+{
+	std::ifstream input(info_path);
+	std::string file;
+	int i = 0;
+
+	switch (BROKERAGE)
+	{
+	case 0:
+		for (std::string line; std::getline(input, line);)
+		{
+			if (i == 0)
+			{
+				ACCOUNT_ID = line;
+			}
+			else if (i == 1)
+			{
+				API_TOKEN = line;
+			}
+			i++;
+		}
+	}
+}
+
 int main()
 {
 	int BROKERAGE = 0;
 	int LIVE_DEMO = 0;
 
-	CERBERUS CERBERUS_OBJ(BROKERAGE,LIVE_DEMO);
+	CERBERUS CERBERUS_OBJ(BROKERAGE, LIVE_DEMO);
+	std::string response = CERBERUS_OBJ.CERBERUS_RISK::init_risk(CERBERUS_OBJ.PATH_STRUCT.ACCOUNT_PATH, CERBERUS_OBJ.API_TOKEN);
 
 	std::cout << CERBERUS_OBJ.ACCOUNT_ID << std::endl;
 	std::cout << CERBERUS_OBJ.API_TOKEN << std::endl;
 
-	std::string response = DownloadJSON(CERBERUS_OBJ.PATH_STRUCT.ACCOUNT_PATH, CERBERUS_OBJ.API_TOKEN);
 
 	std::cout << response;
 
+}
